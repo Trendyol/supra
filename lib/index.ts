445f5119e8cfa8d91b50df74e58ec274ca84eccc
Ahmet Can Güven <ahmetcanguven44@gmail.com>
2019-11-08 09:48:00 +0300

    Opossum temp fix

--- lib/index.ts
+++ lib/index.ts
@@ -4,17 +4,5 @@ import {Http} from "./http";
 const http = new Http();
 const client = new Client(http);
 
-client.request('requestName', 'https://m.trendyol.com', {
-  enabled: true,
-  allowWarmUp: true,
-  httpTimeout: 1000,
-  json: true
-})
-  .then(res => {
-    console.log(typeof res.body)
-  })
-  .catch(err => {
-    console.log('err', err);
-  });
 
 export = client;
